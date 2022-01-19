57eec9d3f3eaa46bbba711ab7b58e77b1790a1a8
fatima tuana <fatima__tuana@hotmail.com>
2022-01-19 12:21:14 +0100

    db update

--- db/webproj (4).sql
+++ db/webproj (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 18. Jan 2022 um 10:33
+-- Erstellungszeit: 19. Jan 2022 um 12:20
 -- Server-Version: 10.4.21-MariaDB
 -- PHP-Version: 8.0.12
 
@@ -33,7 +33,7 @@ CREATE TABLE `news` (
   `content` text NOT NULL,
   `author` varchar(45) DEFAULT NULL,
   `title` varchar(120) NOT NULL,
-  `dateTime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `dateTime` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -41,9 +41,13 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `image`, `content`, `author`, `title`, `dateTime`) VALUES
-(1, 'Unbenannt-2.jpg', 'wfwegweg', 'regrh3', 'test beitrag', '0000-00-00 00:00:00'),
-(2, 'Unbenannt-2.jpg', 'wfwegweg', 'regrh3', 'test beitrag', '0000-00-00 00:00:00'),
-(3, 'Unbenannt-2.jpg', '4wh656j', '65j56', 'rb4h', '0000-00-00 00:00:00');
+(1, 'Unbenannt-2.jpg', 'wfwegweg', 'regrh3', 'test beitrag', 0),
+(2, 'Unbenannt-2.jpg', 'wfwegweg', 'regrh3', 'test beitrag', 0),
+(3, 'Unbenannt-2.jpg', '4wh656j', '65j56', 'rb4h', 0),
+(4, 'wellnessbereich_neu_kamin.jpg', 'Im Dezember 2021 ist unser neuer Wellness-Bereich eröffnet worden. Entspannung pur!', 'Sophia Ehmayer', 'Genießen Sie den neuen Wellness-Bereich!', 1642501674),
+(5, 'covid19_regeln_2021.jpg', 'Zahlreiche europäische Länder verzeichnen derzeit steigende Corona-Fallzahlen – auch Österreich. Um das dynamische Infektionsgeschehen einzudämmen, gelten strenge Corona-Maßnahmen. Für Gastronomie und Beherbergung gilt die 2-G-Regel. Das bedeutet, dass nur Geimpfte oder Genesene Zutritt haben. FFP2-Maskenpflicht gilt in öffentlichen Innenräumen UND im Freien, überall dort, wo ein Zwei-Meter-Abstand zu fremden Personen nicht eingehalten werden kann. Für Aufenthalte aus dringend notwendigen beruflichen Gründen gilt nach wie vor die 3-G-Regel. Für die gastronomischen Einrichtungen des Beherbergungsbetriebs wird aber ein 2-G-Nachweis benötigt', 'Sophia Ehmayer', 'Die neuen Corona-Regeln', 1642501733),
+(6, 'csm_AdinaRezeption-Aufmacherbild-c-AdinaHotels-2640_e6a2f07148.jpg', 'Das Hotel X begann im Jahr 2019 mit der Sanierung von über 50 Hotelzimmer, der Lobby und des Wellness-Bereichs. Außerdem wird an einem Zubau gearbeitet. Geplant sind 30 neue Hotelzimmer. Im Sommer 2022 ist es nun soweit! Die Eröffnung des modernen Zubaus, der renovieren Zimmer, der Lobby und des Wellness-Bereichs ist geplant für 01.07.2022. Buchen Sie schon jetzt ein Zimmer im wunderschönen, neu sanierten Hotel X!', 'Sophia Ehmayer', 'Renovierungsarbeiten bald abgeschlossen!', 1642501771),
+(7, 'hotelzimmer-typ-sternentraum-hoteleinrichtung.jpg', 'Die Hotelzimmer, teilweise mit Schlossblick sind mit ca. 25 m² geräumig konzipiert, hell und freundlich ausgestattet und im modernen Stil eingerichtet. Das Hotelfoyer ist eine großzügig gestaltete All-in-One Symbiose von Rezeption, Frühstückscafe und Lounge/Lobby und Bar. Zusätzlich verfügt das Hotel über zwei kleinere Tagungsräume mit Tageslicht und Klimaanlage. Die Räumlichkeiten sind, je nach Bestuhlung für 10 bis 40 Tagungsgäste geeignet. Das Haus bietet somit hervorragende Optionen für Business- und Individualreisende. Alle Zimmer verfügen über stilvolle Badezimmer mit Dusche und WC, Klimaanlage, TV und High Speed Internet über Wlan. Der Hauptbahnhof ist nur 300 m vom Hotel entfernt und garantiert direkte Anschlüsse und schnelle Verbindungen zu den Flughäfen München und Nürnberg.', 'Sophia Ehmayer', 'Hotel X - ein Ort zum Entspannen', 1642501809);
 
 -- --------------------------------------------------------
 
@@ -67,8 +71,8 @@ CREATE TABLE `tickets` (
 --
 
 INSERT INTO `tickets` (`id`, `user_id`, `info`, `image`, `state`, `title`, `timestamp`, `reply`) VALUES
-(1, 1, 'das ist ein ticket vom user testuser1', 'placeholder.jpg', 'erfolglos geschlossen', 'ticket1', '2022-01-17 22:50:27', NULL),
-(2, 1, 'auch das hier ist ein ticket vom user testuser1', 'placeholder.jpg', 'erfolglos geschlossen', 'ticket2', '2022-01-14 22:43:05', 'fgg'),
+(1, 1, 'das ist ein ticket vom user testuser1', 'placeholder.jpg', 'erfolglos geschlossen', 'ticket1', '2022-01-18 16:17:48', ''),
+(2, 1, 'auch das hier ist ein ticket vom user testuser1', 'placeholder.jpg', 'erfolgreich geschlossen', 'ticket2', '2022-01-19 10:56:27', 'fgg'),
 (3, 2, 'das ist ein ticket vom user testuser2 balaa', 'placeholder2.jpg', 'erfolglos geschlossen', 'ticket3', '2022-01-14 21:05:32', NULL),
 (65, 8, 'Das ist emwifwe', './uploadGuest/2022-01-17_11-09-03unnamed.jpg', 'erfolglos geschlossen', 'Mein Ticket 1', '2022-01-17 22:52:09', 'Es ist uns leider nicht gelungen ..');
 
@@ -99,7 +103,7 @@ INSERT INTO `users` (`id`, `username`, `gender`, `firstname`, `lastname`, `passw
 (2, 'testuser2', NULL, '', '', 'testuser2', 'testuser2@gmail.com', 'guest', 1),
 (3, 'admin', NULL, '', '', 'neues passwort', 'hae@gmail.com', 'admin', 1),
 (5, 'techniker', NULL, '', '', '123456', 'techniker@gmail.com', 'technician', 1),
-(6, 'ema33', 'male', 'adem', 'adem', '$2y$10$aYmt5Kq4Ep9QwINL2h1cEOY5SIYztWZjEAymPyTkcwFmI9Tm.Vxfi', 'adem@gmail.com', 'admin', 0),
+(6, 'ema33', 'male', 'adem', 'adem', '$2y$10$YskHwsOyDqBMXmqNLcN1auoOpmKGa5vsjMYSxER1TEfXIJPsgoWuS', 'adem@gmail.com', 'admin', 0),
 (8, 'user', 'female', 'user', 'user', '$2y$10$Cc93QHWpvAHhaoirXexlAO9uaUE2GtEH0VY/nQPs9htWQXGHmBtve', 'user@gmail.com', 'guest', 0),
 (9, 'techniker1', 'male', 'techniker1', 'techniker1', '$2y$10$sO4PfsnWHuCn2D3yRMgap.8LBFMpSQAyMBqWGWW.LHv75jWazIaFe', 'techniker1@gmail.com', 'technician', 0);
 
@@ -135,7 +139,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT für Tabelle `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT für Tabelle `tickets`
