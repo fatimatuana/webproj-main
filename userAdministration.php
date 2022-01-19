ab57bad923a4f9fc2ccd3c7540b268a30388735d
sophia2802 <93908324+sophia2802@users.noreply.github.com>
2022-01-19 12:54:19 +0100

    Update userAdministration.php

--- userAdministration.php
+++ userAdministration.php
@@ -16,11 +16,22 @@ echo "<tr>";
         echo "<th>Nachname</th>";
         echo "<th>Username</th>";
         echo "<th>Email</th>";
+        echo "<th>Status</th>";
         echo "<th>Edit</th>";
+        
     echo "</tr>";
+
+    
 
 while ($row = $result->fetch_assoc()) { //_assoc works, _object not
 
+    $currState = $row['state'];
+
+    if($currState=='0') {
+        $currState = "aktiv";
+    } elseif($currState=='1') {
+        $currState = "inaktiv";
+    }
     
     echo "<tr>";
        
@@ -29,6 +40,7 @@ while ($row = $result->fetch_assoc()) { //_assoc works, _object not
         echo "<td>" . $row['lastname'] . "</td>";
         echo "<td>" . $row['username'] . "</td>";
         echo "<td>" . $row['email'] . "</td>";
+        echo "<td>" .  $currState . "</td>";
         echo "<td><a href=\"userEdit.php?id=". $row['id'] ."\" >Bearbeiten</a></td>";
     echo "</tr>";
     
